ab94f1a0983afa9cf6850d77a378f3e02daf393b
Ajay Ganapathy <lets.talk@designbyajay.com>
2021-04-26 17:15:33 -0400

    stub out a playwright test
    use `beforeAll`, `beforeEach`, `afterAll` and `afterEach` to set up and tear down playwright

--- tests/unit/example.spec.ts
+++ tests/unit/example.spec.ts
@@ -1,7 +1,21 @@
 import { shallowMount } from '@vue/test-utils';
 import HelloWorld from '@/components/HelloWorld.vue';
 
-describe('HelloWorld.vue', () => {
+import { chromium } from 'playwright';
+
+let browser: any;
+
+beforeAll(async () => {
+  browser = await chromium.launch({ headless: false });
+});
+
+let page: any;
+
+beforeEach(async () => {
+  page = await browser.newPage();
+});
+
+describe('HelloWorld.vue', async () => {
   it('renders props.msg when passed', () => {
     const msg = 'new message';
     const wrapper = shallowMount(HelloWorld, {
@@ -9,4 +23,17 @@ describe('HelloWorld.vue', () => {
     });
     expect(wrapper.text()).toMatch(msg);
   });
+
+  it('should go to a webpage', async () => {
+    await page.goto('https://www.example.com');
+    expect(await page.title()).toBe('Example Domain');
+  });
+});
+
+afterEach(async () => {
+  await page.close();
+});
+
+afterAll(async () => {
+  await browser.close();
 });
