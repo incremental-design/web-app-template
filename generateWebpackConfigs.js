a5ca0e10b3f4718e2576355201c2506173786ed6
Ajay Ganapathy <lets.talk@designbyajay.com>
2021-06-11 19:32:21 -0400

    express serve from memory
    1. `yarn add —dev fs-monkey` (needed to monkey-patch require)
    
    2. `yarn add —dev unionfs` (needed to make require grab from both memory and node_modules)
    
    make an express middleware to serve static files (since serve-static won’t read from memfs)

--- generateWebpackConfigs.js
+++ generateWebpackConfigs.js
@@ -1,13 +1,17 @@
 const Webpack = require('webpack');
-const WebpackDevServer = require('webpack-dev-server');
-const portscanner = require('portscanner');
+// const WebpackDevServer = require('webpack-dev-server');
+// const portscanner = require('portscanner');
 const { createFsFromVolume, Volume } = require('memfs');
+const { patchRequire } = require('fs-monkey');
+const { ufs } = require('unionfs');
+const fs = require('fs');
 const path = require('path');
 const serialize = require('serialize-javascript');
 const express = require('express');
 const { renderToString } = require('@vue/server-renderer');
+const deasync = require('deasync');
 
-(async () => {
+async function setupServer() {
   const webpackConfig = './node_modules/@vue/cli-service/webpack.config.js';
   process.env.NODE_ENV = 'development';
 
@@ -19,7 +23,6 @@ const { renderToString } = require('@vue/server-renderer');
   function clearConfig() {
     // see: https://gist.github.com/joerx/3296d972735adc5b4ec1
     Object.keys(require.cache).forEach((key) => {
-      // console.log(key);
       if (key.toString().includes(webpackConfig.slice(1))) {
         delete require.cache[key];
       }
@@ -52,15 +55,6 @@ const { renderToString } = require('@vue/server-renderer');
   SSRCompiler.outputFileSystem = memoryFilesystem;
   SSRCompiler.outputPath = path.join(__dirname, 'dist', 'server');
 
-  // ClientCompiler.run((clientError) => {
-  //   SSRCompiler.run((SSRError) => {
-  //     console.log('\n\n\n\n');
-  //     console.log(memoryFilesystem.readdirSync(path.join(__dirname, 'dist', 'client')));
-  //     console.log('\n\n\n\n');
-  //     console.log(memoryFilesystem.readdirSync(path.join(__dirname, 'dist', 'server')));
-  //   });
-  // });
-
   const compileClient = () => {
     return new Promise((resolve, reject) => {
       ClientCompiler.run((error, stats) => {
@@ -88,18 +82,80 @@ const { renderToString } = require('@vue/server-renderer');
   await compileClient();
   await compileServer();
 
-  console.log('\n\n\n\n');
-  console.log(memoryFilesystem.readdirSync(path.join(__dirname, 'dist', 'client')));
-  console.log('\n\n\n\n');
-  console.log(memoryFilesystem.readdirSync(path.join(__dirname, 'dist', 'server')));
+  ufs.use(memoryFilesystem).use(fs);
+  patchRequire(ufs); // see: https://github.com/streamich/fs-monkey/blob/master/docs/api/patchRequire.md
+
+  // eslint-ingore-next-line
+  const manifest = require(path.join(__dirname, 'dist', 'server', 'ssr-manifest.json'));
+
+  const server = express();
+
+  const appPath = path.join(__dirname, 'dist', 'server', manifest['app.js']);
+
+  const createApp = require(appPath).default;
+
+  server.use('/img', express.static(path.join(__dirname, './dist/client', 'img')));
+  server.use('/js', express.static(path.join(__dirname, './dist/client', 'js')));
+  server.use('/css', express.static(path.join(__dirname, './dist/client', 'css')));
+  server.use('/favicon.ico', express.static(path.join(__dirname, './dist/client', 'favicon.ico')));
+
+  const serveStaticFromMemoryFilesystem = (request, response, next) => {
+    // switch(request.toString().split('/')){
+    // }
+    // console.log(`the request was ${request.path.split('/')[1]} \n\n`);
+    switch (request.path.split('/')[1]) {
+      case 'js':
+      case 'css':
+        response.send(
+          memoryFilesystem.readFileSync(path.join(__dirname, 'dist', 'client', request.path))
+        );
+        break;
+      case 'img':
+      case 'favicon.ico':
+        response.send(
+          Buffer.from(
+            memoryFilesystem.readFileSync(path.join(__dirname, 'dist', 'client', request.path))
+          )
+        );
+        break;
+      default:
+        next();
+    }
+  };
+
+  server.use(serveStaticFromMemoryFilesystem);
+
+  server.get('*', async (request, response) => {
+    const { app, router, store } = await createApp();
+
+    router.push(request.url);
+    await router.isReady();
+
+    const appContent = await renderToString(app);
+    const renderState = `<script>window.VUEX_SSR_STATE = ${serialize(store.state)}</script>`;
+    // consider putting render state into a VUE_APP_* environment variable, and then interpolating that variable in your public/index.html. See: https://cli.vuejs.org/guide/html-and-static-assets.html#interpolation
+
+    memoryFilesystem.readFile(path.join(__dirname, '/dist/client/index.html'), (error, html) => {
+      if (error) {
+        throw error;
+      }
+      const htmlResponse = html
+        .toString()
+        .replace('<div id="app">', `${renderState}<div id="app">${appContent}`);
+      response.setHeader('Content-Type', 'text/html');
+      response.send(htmlResponse);
+    });
+  });
+
+  server.listen(8080);
 
   // const devServerConfig = generateClientConfig().devServer;
-
-  // console.log(devServerConfig);
 
   // const server = new WebpackDevServer(ClientCompiler, devServerConfig);
 
   // const port = await portscanner.findAPortNotInUse(8080, 9000, '127.0.0.1');
 
   // server.listen(port, '127.0.0.1');
-})();
+}
+
+setupServer();
--- src/entry-server.ts
+++ src/entry-server.ts
@@ -2,6 +2,5 @@ import createApp from './main';
 
 export default function() {
   const { app, router, store } = createApp();
-  console.log('the app was created');
   return { app, router, store };
 }
